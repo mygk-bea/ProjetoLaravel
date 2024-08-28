4eae6755888dc09bf5714912ac143cb836518b8c
mygk-bea <meyaguskobeatriz@gmail.com>
2024-08-28 18:16:13 -0300

    Validação de formulários

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,11 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \App\Models\Contato;
 
 class ContatoController extends Controller
 {
-    public function contato() {
-        var_dump($_POST);
+    public function contato(Request $request) {
+        // dd($request->all());
+        // var_dump($_POST);
+
+        // echo $request->input('nome');
+        // echo '<br>';
+        // echo $request->input('email');
+        // echo '<br>';
+        // echo $request->input('mensagem');
+        // echo '<br>';
+
+        // $contato = new Contato();
+        // $contato->nome = $request->input('nome');
+        // $contato->email = $request->input('email');
+        // $contato->telefone = $request->input('telefone');
+        // $contato->motivo_contato = $request->input('motivo_contato');
+        // $contato->mensagem = $request->input('mensagem');
+        // dd($contato);
+        // $contato->save();
+
+        // $contato = new Contato();
+        // $contato->create($request->all());
+
         return View('site.contato');
+    }
+
+    public function salvar(Request $request) {
+        $request->validate([
+            'nome'=>'required|min:3|max:40',
+            'telefone'=>'required|min:9|max:20',
+            'email'=>'email',
+            'motivo_contato'=>'required',
+            'mensagem'=>'required|max:2000',
+        ]);
+
+        Contato::create($request->all());
+        return redirect()->route('site.principal');
     }
 }
--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -4,24 +4,30 @@
     @csrf
     <div class="mb-3">
         <label for="nome" class="form-label">Nome Completo</label>
-        <input type="text" class="form-control" id="nome" name="nome">
+        <input type="text" class="form-control" id="nome" name="nome" value="{{ old('nome') }}">
+    </div>
+    <div class="mb-3">
+        <label for="telefone" class="form-label">Telefone</label>
+        <input type="text" class="form-control" id="telefone" name="telefone" value="{{ old('telefone') }}">
     </div>
     <div class="mb-3">
         <label for="email" class="form-label">E-mail</label>
-        <input type="text" class="form-control" id="email" name="email">
+        <input type="text" class="form-control" id="email" name="email" value="{{ old('email') }}">
     </div>
     <div class="mb-3">
         <label for="mensagem" class="form-label">Mensagem</label>
-        <input type="text" class="form-control" id="mensagem" name="mensagem">
+        <input type="text" class="form-control" id="mensagem" name="mensagem" value="{{ old('mensagem') }}">
     </div>
     <div class="mb-3">
         <select name="motivo" id="motivo" class="form-select">
             <option selected>Qual o motivo do contato?</option>
-            <option value="1">Reclamação</option>
-            <option value="2">Dúvida</option>
-            <option value="3">Elogio</option>
+            <option value="1" {{ old('motivo_contato') == 1 ? 'selected' : '' }}>Reclamação</option>
+            <option value="2" {{ old('motivo_contato') == 2 ? 'selected' : '' }}>Dúvida</option>
+            <option value="3" {{ old('motivo_contato') == 3 ? 'selected' : '' }}>Elogio</option>
         </select>
     </div>
 
     <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
+
+{{ print_r($errors) }}
